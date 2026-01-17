d57287c15f1e0130cc4f65429cca7313b7435255
ivanhromovyi <ivanh@uia.no>
2026-01-17 20:33:27 +0100

    implement read write list

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -13,6 +13,7 @@ public class Main {
         for (int i = 0; i < 5; i++) {
             new Thread(writer).start();
         }
+
         for (int i = 0; i < 10; i++) {
             new Thread(reader).start();
         }
--- src/main/java/core/basesyntax/ReadWriteList.java
+++ src/main/java/core/basesyntax/ReadWriteList.java
@@ -10,16 +10,30 @@ public class ReadWriteList<E> {
     private ReadWriteLock lock = new ReentrantReadWriteLock();
 
     public void add(E element) {
-        // write your code here
+        try {
+            lock.writeLock().lock();
+            list.add(element);
+        } finally {
+            lock.writeLock().unlock();
+        }
+
     }
 
     public E get(int index) {
-        // write your code here
-        return null;
+        try {
+            lock.readLock().lock();
+            return list.get(index);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public int size() {
-        // write your code here
-        return 0;
+        try {
+            lock.readLock().lock();
+            return list.size();
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 }
